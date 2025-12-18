f34aa37f53eb6cf5f24f12462977c8740a8f0282
Ione-ederkiasko <it@ederkiasko.com>
2025-12-18 14:08:31 +0100

    Update db.py

--- backend/db.py
+++ backend/db.py
@@ -1,8 +1,8 @@
-# # backend/db.py
-# from supabase import create_client
-# import os
+# backend/db.py
+from supabase import create_client
+import os
 
-# supabase = create_client(
-#     os.getenv("SUPABASE_URL"),
-#     os.getenv("SUPABASE_SERVICE_KEY")
-# )
+supabase = create_client(
+    os.getenv("SUPABASE_URL"),
+    os.getenv("SUPABASE_SERVICE_KEY")
+)
